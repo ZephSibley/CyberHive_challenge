9560f245a5d9837d19a58f3e7a77406f56c5798b
Zeph <zeph.sibley@yahoo.com>
2020-07-27 18:56:24 +0100

    variable naming

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def get_processes():
     for proc in psutil.process_iter():
         try:
             # Get process name & pid from process object.
-            processName = proc.name()
-            processID = proc.pid
-            print(processName, ' ::: ', processID)
+            process_name = proc.name()
+            process_id = proc.pid
+            print(process_name, ' ::: ', process_id)
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
