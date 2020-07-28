efdc4726fe048055f11a10df0371924175b12d24
Zeph <zeph.sibley@yahoo.com>
2020-07-28 18:40:59 +0100

    Added origin whitelist middleware

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
+
+ORIGIN_WHITELIST = ['']
+
+
+@app.middleware("http")
+async def check_origin_whitelist(request: Request, call_next):
+    if request.client.host not in ORIGIN_WHITELIST:
+        response = Response(status_code=401)
+    else:
+        response = await call_next(request)
+    return response
 
 
 @app.post("/")
